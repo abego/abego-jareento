46b203d33e4e744ec93ed31fcfe6d8d94b235105
Udo Borkowski <ub@abego-software.de>
2023-10-29 10:29:12 +0100

    refactor!: more consistent, simplified public API, less internal stuff
    - misc

--- src/main/java/org/abego/jareento/javaanalysis/Problems.java
+++ src/main/java/org/abego/jareento/javaanalysis/Problems.java
@@ -3,26 +3,25 @@ package org.abego.jareento.javaanalysis;
 import org.abego.jareento.base.Many;
 
 import java.util.Comparator;
-import java.util.stream.Stream;
 
 public interface Problems extends Many<Problem> {
 
     /**
      * Returns a new {@link Problems} instance with the same {@link Problem}s as
-     * in this object, but sorted as defined by the {@code comparator}.
+     * in this instance, but sorted as defined by the {@code comparator}.
      */
     Problems sorted(Comparator<Problem> comparator);
 
     /**
      * Returns a new {@link Problems} instance with the same {@link Problem}s as
-     * in this object, but sorted by their absolute file path, their line
+     * in this instance, but sorted by their absolute file path, their line
      * number and their description text.
      */
     Problems sortedByFile();
 
     /**
      * Returns a new {@link Problems} instance with the same {@link Problem}s as
-     * in this object, but sorted by their description text, their absolute
+     * in this instance, but sorted by their description text, their absolute
      * file path and their line number.
      */
     Problems sortedByDescription();
