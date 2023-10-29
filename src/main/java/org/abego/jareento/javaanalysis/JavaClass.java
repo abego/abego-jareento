1d81041810f37a26130d5dedd5a4ec3a48c9dbcf
Udo Borkowski <ub@abego-software.de>
2023-10-29 09:48:26 +0100

    refactor!: more consistent, simplified public API, less internal stuff
    - misc

--- src/main/java/org/abego/jareento/javaanalysis/JavaClass.java
+++ src/main/java/org/abego/jareento/javaanalysis/JavaClass.java
@@ -2,6 +2,11 @@ package org.abego.jareento.javaanalysis;
 
 import org.abego.jareento.base.WithId;
 
+/**
+ * A Java class, in all its variants ({@code class}, {@code interface}, 
+ * {@code enum},etc), including primitive types and {@code void}.
+ * 
+ */
 public interface JavaClass extends WithId {
 
     boolean isInterface();
