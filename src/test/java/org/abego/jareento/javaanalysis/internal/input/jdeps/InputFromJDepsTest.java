3747afe9bd97705709a3990d12c41c1da6a74e31
Udo Borkowski <ub@abego-software.de>
2023-11-03 23:09:58 +0100

    test: add InputFromJDepsTest

--- src/test/java/org/abego/jareento/javaanalysis/internal/input/jdeps/InputFromJDepsTest.java
+++ src/test/java/org/abego/jareento/javaanalysis/internal/input/jdeps/InputFromJDepsTest.java
@@ -1,11 +1,55 @@
 package org.abego.jareento.javaanalysis.internal.input.jdeps;
 
+import org.abego.commons.io.FileUtil;
+import org.abego.jareento.javaanalysis.JavaAnalysisProject;
+import org.abego.jareento.javaanalysis.JavaTypes;
+import org.abego.jareento.javaanalysis.internal.JavaAnalysisProjectImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.File;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputFromJDepsTest {
+    @Test
+    void smokeTest(@TempDir File tempDir) {
+        File dotFile = new File(tempDir, "jdeps.dot");
+        File storage = new File(tempDir, "storage");
+        FileUtil.copyResourceToFile(getClass(),
+                "/org/abego/jareento/javaanalysis/internal/input/jdeps/jdeps-sample.dot",
+                dotFile);
+
+        Consumer<String> problemConsumer = p -> {};
+        InputFromJDeps input = InputFromJDeps.newInputFromJDeps(dotFile);
+        JavaAnalysisProject project =
+                JavaAnalysisProjectImpl.newJavaAnalysisProjectFromInput(
+                        storage.toURI(),
+                        input,
+                        new File[0],
+                        new File[0],
+                        problemConsumer);
+        JavaTypes types = project.getTypes();
+
+        assertEquals("""
+                        java.lang.Class
+                        java.lang.Object
+                        java.lang.invoke.MethodHandles$Lookup
+                        java.util.NoSuchElementException
+                        org.abego.commons.annotation.SPI
+                        org.abego.commons.blackboard.Blackboard
+                        org.abego.commons.blackboard.BlackboardDefault
+                        org.abego.commons.diff.FileDiffUtil$DirectoryDifferencesOptions
+                        org.abego.commons.diff.internal.DiffImpl$1
+                        org.abego.commons.diff.internal.DiffImpl$1$1
+                        org.abego.commons.seq.AbstractSeq
+                        org.abego.commons.seq.Seq""",
+                types.idStream().sorted().collect(Collectors.joining("\n")));
+    }
 
     @ParameterizedTest
     @CsvSource({
