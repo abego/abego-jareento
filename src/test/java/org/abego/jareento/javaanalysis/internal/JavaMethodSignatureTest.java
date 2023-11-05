315e1da504507298d5374d31c7068dcd54950069
Udo Borkowski <ub@abego-software.de>
2023-11-05 17:22:25 +0100

    test: more JavaMethodSignature tests

--- src/test/java/org/abego/jareento/javaanalysis/internal/JavaMethodSignatureTest.java
+++ src/test/java/org/abego/jareento/javaanalysis/internal/JavaMethodSignatureTest.java
@@ -1,9 +1,13 @@
 package org.abego.jareento.javaanalysis.internal;
 
 import org.abego.jareento.javaanalysis.JavaAnalysisProject;
+import org.abego.jareento.javaanalysis.JavaMethod;
+import org.abego.jareento.javaanalysis.JavaMethodCallTest;
+import org.abego.jareento.javaanalysis.JavaMethodCalls;
 import org.abego.jareento.javaanalysis.JavaMethodSignature;
 import org.abego.jareento.javaanalysis.JavaMethodSignatures;
 import org.abego.jareento.javaanalysis.JavaType;
+import org.abego.jareento.javaanalysis.JavaTypes;
 import org.abego.jareento.javaanalysis.SampleProjectUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -25,7 +29,7 @@ public class JavaMethodSignatureTest {
                 .sorted()
                 .collect(Collectors.joining("\n"));
     }
-    
+
     @Test
     void intersectedWith(@TempDir File tempDir) {
         JavaAnalysisProject project = SampleProjectUtil.setupSampleProject("calls", tempDir);
@@ -40,5 +44,55 @@ public class JavaMethodSignatureTest {
         assertEquals(
                 "meth1(java.util.function.Consumer)",
                 javaMethodSignaturesText(signatures));
+    }
+
+    @Test
+    void getTypesWithMethod(@TempDir File tempDir) {
+        JavaAnalysisProject project = SampleProjectUtil.setupSampleProject("calls", tempDir);
+
+        JavaMethod meth1Method = project.getMethodWithMethodDeclarator(
+                "calls.CallsSample$SubA#meth1(java.util.function.Consumer):void");
+        JavaMethodSignature meth1Signature = meth1Method.getMethodSignature();
+
+        JavaTypes typesWithMeth1 = meth1Signature.getTypesWithMethod();
+
+        assertEquals("""
+                        calls.CallsSample$Root
+                        calls.CallsSample$SubA""",
+                JavaTypeTest.javaTypesText(typesWithMeth1));
+    }
+
+    @Test
+    void getMethodCalls(@TempDir File tempDir) {
+        JavaAnalysisProject project = SampleProjectUtil.setupSampleProject("calls", tempDir);
+
+        JavaMethod meth1Method = project.getMethodWithMethodDeclarator(
+                "calls.CallsSample$SubA#meth1(java.util.function.Consumer):void");
+        JavaMethodSignature meth1Signature = meth1Method.getMethodSignature();
+
+        JavaMethodCalls callsToMeth1 = meth1Signature.getMethodCalls();
+
+        assertEquals("""
+                        calls.CallsSample$Main#meth3(calls.CallsSample$SubA, java.util.function.Consumer):void
+                        calls.CallsSample$Main#meth4(calls.CallsSample$Root, java.util.function.Consumer):void
+                        calls.CallsSample$SubA#meth3(calls.CallsSample$SubA, java.util.function.Consumer):void
+                        calls.CallsSample$SubA#meth4(calls.CallsSample$Root, java.util.function.Consumer):void""",
+                JavaMethodCallTest.callingMethodsDeclaratorTexts(callsToMeth1));
+    }
+
+    @Test
+    void getMethodCallsToType(@TempDir File tempDir) {
+        JavaAnalysisProject project = SampleProjectUtil.setupSampleProject("calls", tempDir);
+
+        JavaMethod meth1Method = project.getMethodWithMethodDeclarator(
+                "calls.CallsSample$SubA#meth1(java.util.function.Consumer):void");
+        JavaMethodSignature meth1Signature = meth1Method.getMethodSignature();
+
+        JavaMethodCalls callsToMeth1 = meth1Signature.getMethodCallsToType("calls.CallsSample$SubA");
+
+        assertEquals("""
+                        calls.CallsSample$Main#meth3(calls.CallsSample$SubA, java.util.function.Consumer):void
+                        calls.CallsSample$SubA#meth3(calls.CallsSample$SubA, java.util.function.Consumer):void""",
+                JavaMethodCallTest.callingMethodsDeclaratorTexts(callsToMeth1));
     }
 }
